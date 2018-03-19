45acbadc9ab932f5356e72965a9c44b1e66d6712
zahar4uk <zahar4uk.vladislaw2016@yandex.ru>
2018-03-19 15:43:02 +0200

    Added the data in the js route file "Portfolio.js"
    Since I'm running my project on an official guide, I decided to follow the recommendations and do it in the first place in an easier way. So, so far, I do not use Ember data and cli-mirrage

--- app/routes/portfolio.js
+++ app/routes/portfolio.js
@@ -1,4 +1,25 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model (){
+    return [{
+      type: 'print tamplate',
+      image_src: 'Product-image/Example.png'
+    },{
+      type: 'print tamplate',
+      image_src: 'Product-image/Example1.png'
+    },{
+      type: 'print tamplate',
+      image_src: 'Product-image/Example2.png'
+    },{
+      type: 'print tamplate',
+      image_src: 'Product-image/Example3.png'
+    },{
+      type: 'print tamplate',
+      image_src: 'Product-image/Example4.png'
+    },{
+      type: 'print tamplate',
+      image_src: 'Product-image/Example5.png'
+    }];
+  }
 });
