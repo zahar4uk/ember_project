a666d3bdf1140ec1904fe01b792c646e6bac7f99
zahar4uk <zahar4uk.vladislaw2016@yandex.ru>
2018-03-19 17:44:47 +0200

    Added the "image" model Upgraded the hook model and added attributes to the created model

--- app/routes/portfolio.js
+++ app/routes/portfolio.js
@@ -2,24 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model (){
-    return [{
-      type: 'print tamplate',
-      image_src: 'Product-image/Example.png'
-    },{
-      type: 'web tamplate',
-      image_src: 'Product-image/Example1.png'
-    },{
-      type: 'user interface',
-      image_src: 'Product-image/Example2.png'
-    },{
-      type: 'mock up',
-      image_src: 'Product-image/Example3.png'
-    },{
-      type: 'user interface',
-      image_src: 'Product-image/Example4.png'
-    },{
-      type: 'web tamplate',
-      image_src: 'Product-image/Example5.png'
-    }];
+    return this.get('store').findAll('image');
   }
 });
