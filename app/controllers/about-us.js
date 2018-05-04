97a283e5dab51fd099576c2aa21ac3e36e3def33
zahar4uk <35629615+zahar4uk@users.noreply.github.com>
2018-05-04 14:09:01 +0300

    added function and properties in controller

--- app/controllers/about-us.js
+++ app/controllers/about-us.js
@@ -1,4 +1,15 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+    workerName: '',
+    workerTitle: '',
+    workerDescription: '',
+    
+    actions:{
+        changeWorker(name, title, description){
+            this.workerName = name;
+            this.workerTitle = title;
+            this.workerDescription = description;
+        }
+    }
 });
--- app/models/employee.js
+++ app/models/employee.js
@@ -3,4 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
     title: DS.attr('string'),
     src: DS.attr('string'),
+    name: DS.attr('string'),
+    description: DS.attr('string')
 });
