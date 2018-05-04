060314fafa063f44c54fd0888dc07d8ca24d4e9e
zahar4uk <35629615+zahar4uk@users.noreply.github.com>
2018-05-04 15:29:33 +0300

    output information

--- app/controllers/about-us.js
+++ app/controllers/about-us.js
@@ -4,12 +4,14 @@ export default Controller.extend({
     workerName: '',
     workerTitle: '',
     workerDescription: '',
+    imageSrc:'',
     
     actions:{
-        changeWorker(name, title, description){
+        changeWorker(name, title, description, src){
             this.workerName = name;
             this.workerTitle = title;
             this.workerDescription = description;
+            this.imageSrc = src;
         }
     }
 });
