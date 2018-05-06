c88ca49af32dea24e8a495944654fa3c71975634
zahar4uk <35629615+zahar4uk@users.noreply.github.com>
2018-05-06 18:26:14 +0300

    output information and employee at the first page load

--- app/routes/about-us.js
+++ app/routes/about-us.js
@@ -3,5 +3,15 @@ import Route from '@ember/routing/route';
 export default Route.extend({
     model (){
     return this.get('store').findAll('employee');
+  },
+    setupController: function(controller, model) {
+      
+    this._super(controller, model);
+    let modelRecord = this.get('store').peekRecord('employee',0);
+
+    controller.set('workerName',modelRecord.get('name'));
+    controller.set('workerTitle',modelRecord.get('title'));
+    controller.set('workerDescription',modelRecord.get('string'));
+    controller.set('imageSrc',modelRecord.get('src'));
   }
 });
