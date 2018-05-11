aa0365b44535f55b11ab107398cb8f667dc43164
zahar4uk <35629615+zahar4uk@users.noreply.github.com>
2018-05-11 11:51:50 +0300

    Creating a controller for the admin and defining the models

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
-
+import RSVP from 'rsvp';
 export default Route.extend({
+    model() {
+        return RSVP.hash({
+          reviews: this.get('store').findAll('review'),
+          employees: this.get('store').findAll('employee')
+        });
+      }
 });
