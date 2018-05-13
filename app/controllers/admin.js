6cac4726d7c7eb7cce3156dc02ca3e7c7afdc95d
zahar4uk <35629615+zahar4uk@users.noreply.github.com>
2018-05-13 17:30:46 +0300

    implementation of updating records (without modification in mirage)

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -1,9 +1,10 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-  selectType: 'print tamplate',
+  selectType: null,
   imageSrc: '',
-
+  editImgSrc: '',
+  selectedProductId: '',
   actions:{
     changeType(value){
       this.set('selectType', value);
@@ -19,6 +20,17 @@ export default Controller.extend({
         msrc: '(optional) larger image'
       });
       store.save();
+    },
+    selectProduct(src, type){
+      this.set('selectType',type);
+      this.set('editImgSrc',src);
+      this.set('selectedProductId',src);
+    },
+    saveChange(){
+      this.get('store').findRecord('portfolio', this.get('selectedProductId')).then(function(portfolio) {
+        portfolio.set('type',this.get('editImgSrc'));
+        portfolio.set('src',this.get('selectType'));
+      });
     }
   }
 });
